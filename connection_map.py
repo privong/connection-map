bbac29592b83da205826da355f2d3bc3897371eb
George C. Privon <accounts5@privon.com>
2017-05-23 22:53:25 -0400

    clarify license

--- connection_map.py
+++ connection_map.py
@@ -2,7 +2,23 @@
 """
 Periodically get a list of IP Connections and map them out.
 
-Copyright 2017 George C. Privon
+Some code from the cartopy documentation.
+http://scitools.org.uk/cartopy/docs/latest/
+
+Other code Copyright 2017 George C. Privon
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import subprocess
